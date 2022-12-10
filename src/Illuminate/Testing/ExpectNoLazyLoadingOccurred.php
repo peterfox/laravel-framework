a11433f2ec82a4e2365702cb79878bde7e01aa84
Peter Fox <peter.fox@peterfox.me>
2022-12-10 15:06:48 +0000

    better defensive programming if there is an issue finding the right trace

--- src/Illuminate/Testing/ExpectNoLazyLoadingOccurred.php
+++ src/Illuminate/Testing/ExpectNoLazyLoadingOccurred.php
@@ -15,13 +15,18 @@ class ExpectNoLazyLoadingOccurred
      * @return void
      *
      * @throws \PHPUnit\Framework\AssertionFailedError
+     * @throws \RuntimeException
      */
     public function __invoke($model, $key)
     {
         $trace = debug_backtrace(DEBUG_BACKTRACE_PROVIDE_OBJECT | DEBUG_BACKTRACE_IGNORE_ARGS, 20);
 
-        ['file' => $file, 'line' => $line] = $this->resolveTrace($trace, $model);
+        $caller = $this->resolveTrace($trace, $model);
+        if ($caller === null || !isset($caller['file'], $caller['line'])) {
+            throw new \RuntimeException('Unable to determine the caller of the lazy loading.');
+        }
 
+        ['file' => $file, 'line' => $line] = $caller;
         $modelName = $model::class;
 
         throw new AssertionFailedError("{$modelName} attempted to lazy load attribute {$key} in {$file}:{$line}");
